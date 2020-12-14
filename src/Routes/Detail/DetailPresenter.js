d3136b741fb88881af753f33824bf1ba0aae0941
hyesech <hyesech@gmail.com>
2020-12-14 16:46:27 +0900

    Detail Page: backdrop

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -2,7 +2,35 @@ import React from "react";
 import propTypes from "prop-types";
 import styled from "styled-components";
 
-const DetailPresenter = ({ result, error, loading }) => null;
+import Loader from "../../Components/Loader";
+
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImg});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const DetailPresenter = ({ result, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImg={`https://image.tmdb.org/t/p/original${result.Backdrop_path}`}
+      />
+    </Container>
+  );
 
 DetailPresenter.prototype = {
   result: propTypes.object,
